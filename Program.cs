80009c62f1c382b4713e3f462c823ebda582c510
GParcade <ddann700@gmail.com>
2022-01-17 17:25:50 +0200

    More tests for different types

--- Program.cs
+++ Program.cs
@@ -4,9 +4,11 @@
 Enbt enbt = new Dictionary<string, Enbt>();
 
 enbt["hello"] = 123;
-enbt["aaa"] = 343;
+enbt["aaa"] = 343.21;
 enbt["ssss"] = Enbt.Empty;
-enbt["yyyyy"] = 263;
+enbt["yyyyy"] = 263.123f;
+enbt["bbbbb"] = true;
+enbt["sfassa"] = Guid.NewGuid();
 Enbt arr = enbt["yawn"] = new List<Enbt>(4);
 
 arr[1] = 325;
@@ -20,6 +22,9 @@ using (EnbtStream writer = new("test.enbt", FileMode.Create))
 Console.WriteLine(enbt);
 
 
+
+///{"hello":123, "aaa":343.21, "ssss":, "yyyyy":263.123, "bbbbb":True, "sfassa":04e7736f-8a63-4ca2-bbd9-1027d49170bb, "yawn":[, 325, , 436]}
+
 //// test in c++ passed
 //std::fstream fs;
 //fs.open("test.enbt", std::ios::in);
@@ -32,18 +37,22 @@ Console.WriteLine(enbt);
 //
 //
 //std::cout << (int)to_test["hello"] << std::endl;
-//std::cout << (int)to_test["aaa"] << std::endl;
+//std::cout << (double)to_test["aaa"] << std::endl;
 //std::cout << (int)to_test["ssss"] << std::endl;
-//std::cout << (int)to_test["yyyyy"] << std::endl;
+//std::cout << (float)to_test["yyyyy"] << std::endl;
+//std::cout << (bool)to_test["bbbbb"] << std::endl;
+//std::cout << (ENBT::UUID)to_test["sfassa"] << std::endl;
 //std::vector<ENBT> arr = to_test["yawn"];
 //for (auto & it : arr)
 //    std::cout << (int)it << std::endl;
 //
 //// console output
 //123
-//343
+//343.21
 //0
-//263
+//263.123
+//1
+//04e7736f-8a63-4ca2-bbd9-1027d49170bb
 //0
 //325
 //0
